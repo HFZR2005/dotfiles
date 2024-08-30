c2c50b9f1dfe31568353b2e8e8cc77b24878191f
Hifzur <hifzur@Hifzurs-Air.cust.communityfibre.co.uk>
2024-08-30 16:33:13 +0100

    Initialis

---
+++ .config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,41 @@
+
+return {
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.startify")
+
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
+
+    -- Ensure that the header and buttons are centered
+    local function center_content(section)
+      section.opts.position = "center"
+    end
+
+    center_content(dashboard.section.header)
+
+    -- Set up the dashboard
+    alpha.setup(dashboard.opts)
+  end,
+}
+
